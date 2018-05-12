0711c3f285e57b1018904bb28b72a8291a37b1ca
Peng Li <lipengshopping@gmail.com>
2018-05-12 13:36:20 -0700

    data loader

--- model/data_loader.py
+++ model/data_loader.py
@@ -5,6 +5,15 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import numpy as np
 
+def collate_fn(batch):
+    # To solve the variable size problem of y
+    # Override the default_collate of dataloader
+    # Return x:(batch_size, C, H, W) tensor
+    #        y:(batch_size,) list, each item 
+    x = torch.stack([item[0] for item in batch])
+    y = [torch.from_numpy(item[1]) for item in batch]
+    return [x, y]
+
 class DetectionDataset(Dataset):
     def __init__(self, x_dir, y_dir, transform):
         self.X = np.load(x_dir)
@@ -43,10 +52,10 @@ def fetch_dataloader(types, data_dir, params):
             y_dir = data_dir + 'Y_' + split + '.npy'
 
             dl = DataLoader(DetectionDataset(x_dir, y_dir, transformer), batch_size=params.batch_size, shuffle=True, 
-                            num_workers=params.num_workers, pin_memory=params.cuda)
+                            num_workers=params.num_workers, pin_memory=params.cuda, collate_fn = collate_fn)
             dataloaders[split] = dl
     return dataloaders
-
+'''
 class params(object):
     def __init__(self):
         self.num_workers = 4
@@ -58,5 +67,6 @@ data_dir = '../data/GTSDB/'
 test_dataloader = fetch_dataloader(['train'], data_dir, params)
 train_loader = test_dataloader['train']
 for x, y in train_loader:
-    print(x.shape, len(y))
-
+    print(x[0], y[0])
+    break
+'''
